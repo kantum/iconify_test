11cb666f9f47edd4ac1df9b96c5c3b1b2347a71b
kantum <42kantum@gmail.com>
2024-08-25 05:44:33 +0200

    fix: update to 0.5.1 and add stylsheet ref

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule IconifyTest.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:iconify_ex, "~> 0.5.0"}
+      {:iconify_ex, "~> 0.5.1"}
     ]
   end
 
